c8d920579f45c290bf51fa5077c7d5158d6faae2
Jean-MarcG <gagnebien.jeanmarc@gmail.com>
2023-10-12 13:59:19 +0200

    feat: update front-page to forward

---
+++ .history/front-page_20231011180938.php
@@ -0,0 +1,172 @@
+<?php get_header() ?>
+
+
+<section class="home section" id="home">
+  <div class="hero__container container">
+
+    <img src="/wp-content/uploads/logo-hero-75x58-1.png" alt="">
+    <div class="hero__data">
+      <span class="section__title hero__title">
+        <span>Roulez</span>, on
+        <br>
+        s'occupe de <u>vous</u>
+      </span>
+    </div>
+  </div>
+
+  <div class="home__container container grid">
+    <div class="home__data">
+      <h1 class="home__title">Auto-École ABEL : Votre Partenaire de Confiance pour le Permis de Conduire à Reims et Taissy depuis 1976</h1>
+      <p class="home__description">
+        Formation à la conduite de qualité depuis 1976
+      </p>
+    </div>
+  </div>
+</section>
+
+<section class="coneShaped section" id="coneShaped">
+  <div class="coneShaped__container container grid">
+    <div class="coneShaped__data">
+      <span class="section__subtitle">
+        Nos spécialités
+      </span>
+      <h2 class="section__title coneShapede__title">
+        Pièces montées en cône sans nougatine
+      </h2>
+      <p class="coneShaped__description">
+        Possibilité de choux ou macarons, <strong>10 shekels/unité</strong>. Choux garnis d’une véritables crème onctueuse avec choix de différents goûts.
+        <br>
+        Commande minimum 30 personnes et supplément pour décor personnalisé.
+      </p>
+      <p class="coneShaped__description">
+        Conseil : 2 à 3 unités par personne
+      </p>
+      <a href="/pieces-montees-en-cone-sans-nougatine/" class="cta">
+        Découvrir <i class="ri-arrow-right-line"></i>
+      </a>
+    </div>
+    <img src="/wp-content/uploads/PHOTO-2023-06-28-11-03-39-copie-1.jpg" alt="" class="coneShaped__img">
+  </div>
+</section>
+
+<section class="croquembouche section" id="croquembouche">
+  <div class="croquembouche__container container grid">
+    <div class="croquembouche__data">
+      <span class="section__subtitle">
+        Nos spécialités
+      </span>
+      <h2 class="section__title croquembouche__title">
+        Croquembouche nougatine
+      </h2>
+      <p class="croquembouche__description">
+        Possibilité de mélanger choux et macarons, <strong>20 shekels l’unité</strong> par personne.
+        <br>Supplément pour décor personnalisé.
+      </p>
+      <p class="croquembouche__description">
+        Conseil : 2 à 3 unités par personne
+      </p>
+      <a href="/croquembouche-nougatine/" class="cta">
+        Découvrir <i class="ri-arrow-right-line"></i>
+      </a>
+    </div>
+    <img src="/wp-content/uploads/croquembouche-brice-min-1.jpg" alt="" class="croquembouche__img">
+  </div>
+</section>
+
+
+<section class="customized section" id="customized">
+  <div class="customized__container container grid">
+    <div class="customized__data">
+      <span class="section__subtitle">
+        Nos spécialités
+      </span>
+      <div class="section__title">
+        <h2>Gâteau personnalisé</h2>
+      </div>
+      <p class="customized__description">
+        Laissez libre votre imagination, choisissez votre thème et nous serons ravis de le réaliser.
+        <br>
+        <strong>25 shekels la part</strong>, commande minimum 15 personnes.
+        <br>
+        Gâteau monté avec génoise moelleuse nature ou chocolat agrémenté de différents goûts de crèmes onctueuses et de coulis fruités.
+      </p>
+      <a href="/gateau-personnalise/" class="cta">
+        Découvrir <i class="ri-arrow-right-line"></i>
+      </a>
+    </div>
+    <img src="/wp-content/uploads/customized-brice.png" alt="" class="customized__img">
+  </div>
+</section>
+
+<section class="numberCake section" id="numberCake">
+  <div class="numberCake__container numberCake__content container grid">
+    <img src="/wp-content/uploads/number-cake.png" alt="" class="numberCake__img">
+    <div class="numberCake__data">
+      <span class="section__subtitle">numberCake</span>
+      <h2 class="section__title">
+        Number Cake
+      </h2 class="numberCake__description">
+      <p class="numberCake__description">
+        Quel est votre chiffre préféré!?
+        <br>
+        Pâte sucrée nature ou chocolat surmontée d’une crème onctueuse : vanille, chocolat, citron, etc... à votre choix.
+        <br>
+        Décor personnalisé sur-mesure à partir de <strong>20 shekels la part</strong>, commande de 15 personnes minimum.
+      </p>
+      <a href="/number-cake/" class="cta">
+        Découvrir <i class="ri-arrow-right-line"></i>
+      </a>
+    </div>
+  </div>
+</section>
+
+<section class="israel section" id="israel">
+  <div class="israel__container container grid">
+    <div class="israel__data">
+      <span class="section__subtitle">
+        savoir-faire
+      </span>
+      <div class="section__title">
+        <h2>Croquembouche et cake Israël</h2>
+      </div>
+      <p class="israel__description">
+        Bienvenue chez Brice Pâtisserie, votre adresse gourmande à Jérusalem pour des délices sucrés qui émerveilleront les mariages !
+        <br> Plongez dans l'univers envoûtant de nos Wedding Cake,Pièces montées en cône sans nougatine, Croquembouches ou Number cake. Offrez aux invités une expérience gustative exceptionnelle.
+        <br>
+        Chez Brice, nous mettons un point d'honneur à allier les saveurs traditionnelles d'Israël à notre savoir-faire français en pâtisserie.
+        <br>
+        Chaque création est confectionnée avec passion et des ingrédients locaux soigneusement sélectionnés. Faites de votre mariage un moment inoubliable en choisissant Brice Pâtisserie pour sublimer vos festivités avec nos délicieuses créations sucrées.
+        <br>Découvrez la magie de nos desserts !
+      </p>
+    </div>
+    <img src="/wp-content/uploads/cake-israel-min.jpg" alt="" class="israel__img">
+  </div>
+</section>
+
+<section class="about section" id="about">
+  <div class="about__container container grid">
+    <div class="about__data">
+      <span class="section__subtitle">
+        about
+      </span>
+      <div class="section__title">
+        <h2>Qui sommes-nous ?</h2>
+      </div>
+      <p class="about__description">
+        Notre spécialité? Les pièces montées croquembouche. Avec amour et passion, nous créons des œuvres d'art comestibles qui marqueront les esprits de vos invités.
+        <br>
+        Nous sommes fiers de dire que chacune de nos pièces montées est une création unique, pensée et réalisée sur mesure pour votre événement spécial. Nous ne travaillons qu'avec les ingrédients les plus nobles pour donner vie à nos créations.
+        <br>
+        En tant que pâtissier spécialisé en pièces montées croquembouche, nous sommes constamment en train de relever de nouveaux défis – des bases traditionnelles aux créations modernes, en passant par tous les styles de décoration possibles. Nous aimons donner vie aux idées les plus folles pour rendre votre événement inoubliable.
+        <br>
+        Nous savons que les détails comptent, c'est pourquoi nous sommes également spécialisés en décorations artistiques personnalisées pour nos clients. Nous travaillons directement avec vous pour créer une pièce unique et sur mesure qui sera en parfait accord avec vos attentes.
+        <br>
+        En fin de compte, l'objectif de toute pièce montée croquembouche est l'émotion de marquer les esprits et d'apporter de la joie à tous ceux qui y goûteront. Grâce à notre expertise et notre passion.
+      </p>
+    </div>
+    <img src="/wp-content/uploads/about-brice-min.jpg" alt="" class="customized__img">
+  </div>
+</section>
+
+
+<?php get_footer() ?>
---
+++ .history/front-page_20231012122512.php
@@ -0,0 +1,196 @@
+<?php get_header() ?>
+
+
+<section class="home section" id="home">
+  <div class="hero__container container">
+
+    <img src="/wp-content/uploads/logo-hero-75x58-1.png" alt="">
+    <div class="hero__content">
+      <span class="section__title hero__title">
+        <span>Roulez</span>, on
+        <br>
+        s'occupe de <u>vous</u>
+      </span>
+    </div>
+  </div>
+
+  <div class="home__container container grid">
+    <div class="home__content">
+      <h1 class="home__title">Auto-École ABEL : Votre Partenaire de Confiance pour le Permis de Conduire à Reims et Taissy depuis 1976</h1>
+      <p class="home__description">
+        Formation à la conduite de qualité depuis 1976
+      </p>
+    </div>
+  </div>
+</section>
+
+<section class="formation section" id="formation">
+  <div class="formation__container container grid">
+    <div class="formation__content">
+      <h2 class="section__title formation__title">Nous sommes au service de votre formation</h2>
+      <div class="formation__description">
+        <p>
+          Depuis 1976, l’Auto-école ABEL, c’est bien entendu une moustache, et des voitures rouges, et en plus du sourire, un réel savoir-faire pour vous former au code et à la conduite dans Reims et Taissy.
+        </p>
+      </div>
+    </div>
+    <img src="/wp-content/uploads/formation-right-abel.jpg" alt="" class="formation__img formation__img-right">
+  </div>
+  <div class="formation__container container grid">
+    <img src="/wp-content/uploads/formation-left-abel.jpg" alt="" class="formation__img formation__img-left">
+    <div class="formation__content">
+      <div class="formation__description">
+        <p><strong>Notre objectif, votre réussite et votre sécurité!</strong></p>
+        <p>Pour cela, une équipe expérimentée à vos côtés dans nos quatre agences à proximité du centre ville de Reims avec les agences Venise, Courlancy, Hauts de Murigny, et Taissy toutes tournées vers votre réussite au permis de conduire .</p>
+        <p>Une pédagogie adaptée pour un parcours de formation personnalisée au permis B en traditionnel ou en conduite accompagnée à Reims et Taissy.</p>
+        <p>Une formation au code de la route à votre rythme avec un bouquin comme papa et maman (pour lire jusqu’au bout de la nuit), une salle de code pour venir vous entraîner entre copains avec ou sans Moniteur (voir les horaires en agence) et bien entendu un accès de code en ligne utilisable sur smartphone, tablette ou ordinateur avec suivi de l’équipe diplômée, pour vous entraîner au code 7/7 24/24 😉</p>
+      </div>
+    </div>
+  </div>
+  <div class="formation__cta container">
+    <a href="#" class="cta cta--red">Nous contacter</a>
+  </div>
+  <div class="formation__badge">
+    <img src="/wp-content/uploads/badge-abel.png" alt="">
+  </div>
+</section>
+
+<section class="package section" id="package">
+  <div class="package__container container grid">
+    <div class="package__content">
+      <h2 class="section__title package__title">
+        Des forfaits
+        <br>
+        adaptés à votre apprentisage
+      </h2>
+      <p class="package__subtitle">
+        Évaluation préalable sur rdv à l’inscription dans le but de définir le volume de formation : séance d’évaluation 45€ ttc
+      </p>
+      <div class="package__cards">
+        <div class="package__card">
+          <div class="package__card--title">permis b et aac</div>
+          <div class="package__card--content">
+            <p>Formation au Code de la Route</p>
+            <p>inclus:
+              <br>Présentiel
+              <br>et
+              <br>Distanciel
+            </p>
+          </div>
+          <div class="package__card--cta">
+            <a href="#" class="cta cta--yellow">en savoir +</a>
+          </div>
+        </div>
+        <div class="package__card">
+          <div class="package__card--title">Conduite accompagnée</div>
+          <div class="package__card--content">
+            <p>Formation au Code de la Route</p>
+            <p>et à la conduite</p>
+            <p>dès 15 ans</p>
+          </div>
+          <div class="package__card--cta">
+            <a href="#" class="cta cta--yellow">en savoir +</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="customized section" id="customized">
+  <div class="customized__container container grid">
+    <div class="customized__content">
+      <span class="section__subtitle">
+        Nos spécialités
+      </span>
+      <div class="section__title">
+        <h2>Gâteau personnalisé</h2>
+      </div>
+      <p class="customized__description">
+        Laissez libre votre imagination, choisissez votre thème et nous serons ravis de le réaliser.
+        <br>
+        <strong>25 shekels la part</strong>, commande minimum 15 personnes.
+        <br>
+        Gâteau monté avec génoise moelleuse nature ou chocolat agrémenté de différents goûts de crèmes onctueuses et de coulis fruités.
+      </p>
+      <a href="/gateau-personnalise/" class="cta">
+        Découvrir <i class="ri-arrow-right-line"></i>
+      </a>
+    </div>
+    <img src="/wp-content/uploads/customized-brice.png" alt="" class="customized__img">
+  </div>
+</section>
+
+<section class="numberCake section" id="numberCake">
+  <div class="numberCake__container numberCake__content container grid">
+    <img src="/wp-content/uploads/number-cake.png" alt="" class="numberCake__img">
+    <div class="numberCake__content">
+      <span class="section__subtitle">numberCake</span>
+      <h2 class="section__title">
+        Number Cake
+      </h2 class="numberCake__description">
+      <p class="numberCake__description">
+        Quel est votre chiffre préféré!?
+        <br>
+        Pâte sucrée nature ou chocolat surmontée d’une crème onctueuse : vanille, chocolat, citron, etc... à votre choix.
+        <br>
+        Décor personnalisé sur-mesure à partir de <strong>20 shekels la part</strong>, commande de 15 personnes minimum.
+      </p>
+      <a href="/number-cake/" class="cta">
+        Découvrir <i class="ri-arrow-right-line"></i>
+      </a>
+    </div>
+  </div>
+</section>
+
+<section class="israel section" id="israel">
+  <div class="israel__container container grid">
+    <div class="israel__content">
+      <span class="section__subtitle">
+        savoir-faire
+      </span>
+      <div class="section__title">
+        <h2>package et cake Israël</h2>
+      </div>
+      <p class="israel__description">
+        Bienvenue chez Brice Pâtisserie, votre adresse gourmande à Jérusalem pour des délices sucrés qui émerveilleront les mariages !
+        <br> Plongez dans l'univers envoûtant de nos Wedding Cake,Pièces montées en cône sans nougatine, packages ou Number cake. Offrez aux invités une expérience gustative exceptionnelle.
+        <br>
+        Chez Brice, nous mettons un point d'honneur à allier les saveurs traditionnelles d'Israël à notre savoir-faire français en pâtisserie.
+        <br>
+        Chaque création est confectionnée avec passion et des ingrédients locaux soigneusement sélectionnés. Faites de votre mariage un moment inoubliable en choisissant Brice Pâtisserie pour sublimer vos festivités avec nos délicieuses créations sucrées.
+        <br>Découvrez la magie de nos desserts !
+      </p>
+    </div>
+    <img src="/wp-content/uploads/cake-israel-min.jpg" alt="" class="israel__img">
+  </div>
+</section>
+
+<section class="about section" id="about">
+  <div class="about__container container grid">
+    <div class="about__content">
+      <span class="section__subtitle">
+        about
+      </span>
+      <div class="section__title">
+        <h2>Qui sommes-nous ?</h2>
+      </div>
+      <p class="about__description">
+        Notre spécialité? Les pièces montées package. Avec amour et passion, nous créons des œuvres d'art comestibles qui marqueront les esprits de vos invités.
+        <br>
+        Nous sommes fiers de dire que chacune de nos pièces montées est une création unique, pensée et réalisée sur mesure pour votre événement spécial. Nous ne travaillons qu'avec les ingrédients les plus nobles pour donner vie à nos créations.
+        <br>
+        En tant que pâtissier spécialisé en pièces montées package, nous sommes constamment en train de relever de nouveaux défis – des bases traditionnelles aux créations modernes, en passant par tous les styles de décoration possibles. Nous aimons donner vie aux idées les plus folles pour rendre votre événement inoubliable.
+        <br>
+        Nous savons que les détails comptent, c'est pourquoi nous sommes également spécialisés en décorations artistiques personnalisées pour nos clients. Nous travaillons directement avec vous pour créer une pièce unique et sur mesure qui sera en parfait accord avec vos attentes.
+        <br>
+        En fin de compte, l'objectif de toute pièce montée package est l'émotion de marquer les esprits et d'apporter de la joie à tous ceux qui y goûteront. Grâce à notre expertise et notre passion.
+      </p>
+    </div>
+    <img src="/wp-content/uploads/about-brice-min.jpg" alt="" class="customized__img">
+  </div>
+</section>
+
+
+<?php get_footer() ?>
