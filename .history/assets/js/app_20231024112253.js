32c1d95db8cff4affb6b3385351253e634ce3374
Jean-MarcG <gagnebien.jeanmarc@gmail.com>
2023-10-24 14:14:48 +0200

    feat : beta

---
+++ .history/assets/js/app_20231024112253.js
@@ -0,0 +1,101 @@
+/**
+* File primary-navigation.js.
+*
+* Required to open and close the mobile navigation.
+*/
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+    : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 400,
+    // reset: true, // Animation repeat
+})
+
+sr.reveal('.hero__container, .formation__container, .package__title, .package__subtitle, .forward__container, .agences, .footer__before, .footer__after', {origin: 'bottom'})
+sr.reveal('.permisPresentation', {origin: 'bottom'})
+sr.reveal('.package__card--left', {origin: 'left'})
+sr.reveal('.package__card--right', {origin: 'right'})
+
+/*=============== SCROLL TEXT ANIMATION ===============*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollingText = document.querySelector(".scrolling-text__content");
+    
+    // Vérifiez si le texte existe
+    if (scrollingText) {
+        const text = scrollingText.textContent;
+        scrollingText.innerHTML += text;
+    }
+});
+
+/*=============== COPY TEXT ===============*/
+
+const copyEmail = document.getElementById("copyEmail");
+
+copyEmail.addEventListener("click", function (Event) {
+    navigator.clipboard.writeText("info@autoecoleabel.fr").then(() => {
+        document.querySelector('.contact__left button').innerHTML = 'info@autoecoleabel.fr <svg width="22" height="17" viewBox="0 0 22 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 7.95349L8.02326 13.9767L20.5 1.5" stroke="#D93513" stroke-width="3"/></svg>';
+    })
+});
+
+// Signature
+if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+    var args = ['\n %c with <3 by @jmg \u2730 http://www.2212.fr/ \n\n','background: #1C4F96; padding:5px 0;color: #333333;'];
+    window.console.log.apply(console, args);
+} else if (window.console) {
+    window.console.log('@jmg - http://www.2212.fr/');
+}
+
+
---
+++ .history/front-page_20231024121133.php
@@ -0,0 +1,229 @@
+<?php get_header() ?>
+
+<section class="hero section" id="hero">
+  <img src="/wp-content/uploads/moustache-abel.png" alt="" class="mustache">
+  <div class="hero__container container">
+    <div class="hero__content">
+      <span class="hero__subtitle">
+        <span>Prenez le volant</span>, on
+        <br>
+        s'occupe de <u>vous</u>
+      </span>
+      <h1 class="hero__title">Auto-École ABEL : Votre Partenaire de Confiance pour le Permis de Conduire
+        <br> à Reims et Taissy
+      </h1>
+      <p class="hero__description">Formation à la conduite de qualité depuis 1976</p>
+    </div>
+  </div>
+</section>
+
+<section class="formation section" id="formation">
+  <div class="formation__container container grid">
+    <div class="formation__content">
+      <h2 class="section__title formation__title">Nous sommes au service de votre formation</h2>
+      <div class="formation__description">
+        <p>
+          Depuis 1976, l’Auto-école ABEL, c’est bien entendu une moustache, et des voitures rouges, et en plus du sourire, un réel savoir-faire pour vous former au code et à la conduite dans Reims et Taissy.
+        </p>
+      </div>
+    </div>
+    <div class="formation__block"></div>
+    <img src="/wp-content/uploads/formation-right-abel.jpg" alt="" class="formation__img formation__img-right">
+  </div>
+  <div class="formation__container formation__container--two container grid">
+    <img src="/wp-content/uploads/formation-left-abel.jpg" alt="" class="formation__img formation__img-left">
+    <div class="formation__block"></div>
+    <div class="formation__content">
+      <div class="formation__description">
+        <p><strong>Notre objectif, votre réussite et votre sécurité!</strong></p>
+        <p>Pour cela, une équipe expérimentée à vos côtés dans nos quatre agences à proximité du centre ville de Reims avec les agences Venise, Courlancy, Hauts de Murigny, et Taissy toutes tournées vers votre réussite au permis de conduire .</p>
+        <p>Une pédagogie adaptée pour un parcours de formation personnalisée au permis B en traditionnel ou en conduite accompagnée à Reims et Taissy.</p>
+        <p>Une formation au code de la route à votre rythme avec un bouquin comme papa et maman (pour lire jusqu’au bout de la nuit), une salle de code pour venir vous entraîner entre copains avec ou sans Moniteur
+          <a href="#agences"> (voir les horaires en agence)</a> et bien entendu un accès de code en ligne utilisable sur smartphone, tablette ou ordinateur avec suivi de l’équipe diplômée, pour vous entraîner au code 7/7 24/24 😉.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="formation__cta container">
+    <a href="#" class="cta cta--red">Prendre rendez-vous</a>
+  </div>
+  <div class="formation__badge">
+    <img src="/wp-content/uploads/badge-abel.png" alt="">
+  </div>
+</section>
+
+<section class="package section" id="package">
+  <div class="package__container container grid">
+    <div class="package__content">
+      <h2 class="section__title package__title">
+        Des forfaits
+        <br>
+        adaptés à votre apprentissage
+      </h2>
+      <p class="package__subtitle">
+        Évaluation préalable sur rdv à l’inscription dans le but de définir le volume de formation : séance d’évaluation 45€ ttc
+      </p>
+      <div class="package__card--wrapper">
+        <div class="package__card package__card--left">
+          <div class="package__card--title">permis b et aac</div>
+          <div class="package__card--content">
+            <img src="/wp-content/uploads/permis-illustration-1.png" alt="">
+          </div>
+          <div class="package__card--cta">
+            <a href="#" class="cta cta--yellow">En savoir +</a>
+          </div>
+        </div>
+        <div class="package__card package__card--right">
+          <div class="package__card--title">Conduite accompagnée</div>
+          <div class="package__card--content">
+            <img src="/wp-content/uploads/logo-conduite-accompagnee-1.png" alt="">
+          </div>
+          <div class="package__card--cta">
+            <a href="#" class="cta cta--yellow">En savoir +</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="package__cta container">
+    <a href="#" class="cta cta--red">Prendre rendez-vous</a>
+  </div>
+</section>
+
+<div class="scrolling-text__wrapper">
+  <div class="scrolling-text__inner-wrapper">
+    <span>Notre objectif, votre réussite et votre sécurité!</span>
+    <span>Notre objectif, votre réussite et votre sécurité!</span>
+    <span>Notre objectif, votre réussite et votre sécurité!</span>
+    <span>Notre objectif, votre réussite et votre sécurité!</span>
+  </div>
+</div>
+
+
+<section class="forward section" id="forward">
+  <div class="forward__container container grid">
+    <img src="/wp-content/uploads/push-left-abel.jpg" alt="" class="forward__img forward__img--one">
+    <div class="forward__content">
+      <div class="forward__title">
+        <h2>nous venons vous chercher à l'endroit qui vous convient le mieux</h2>
+      </div>
+      <p class="forward__description">
+        Pour rendre les choses encore plus simples, nous venons même vous chercher chez vous, au lycée, ou à l'endroit qui vous convient le mieux. Votre confort est notre priorité. <br>
+      </p>
+      <div class="forward__cta">
+        <a href="/contact" class="cta cta--red">Nous contacter</a>
+      </div>
+    </div>
+  </div>
+  <div class="forward__container container grid">
+    <div class="forward__content">
+      <div class="forward__title">
+        <h2>des moniteurs expérimentés</h2>
+      </div>
+      <p class="forward__description">
+        Nos formateurs, diplômés d'État, ont une passion pour l'enseignement de la conduite. Ils allient savoir-faire et pédagogie pour vous guider vers la réussite de votre permis de conduire. Ils sont à l'écoute de vos besoins. </p>
+      <div class="forward__cta">
+        <a href="/contact" class="cta cta--red">Nous contacter</a>
+      </div>
+    </div>
+    <img src="/wp-content/uploads/push-right-abel.jpg" alt="" class="forward__img">
+  </div>
+</section>
+
+<section class="agences section" id="agences">
+  <h2 class="agences__title">
+    4 agences pour votre formation
+  </h2>
+  <div class="agences__container">
+    <div class="agences__content agences__content-one">
+      <div>
+        <span class="agences--title">abel hauts de murigny</span>
+        <span class="agences--adresses">
+          1 place Toulouse Lautrec
+          <br>
+          51100 reims
+          <br>
+          <a href="tel:+0326360536">03.26.36.05.36</a>
+          <br>
+          <span>
+            Du lundi au vendredi de 14h à 19h
+          </span>
+          <br>
+          <span>
+            Mercredi et Jeudi de 10h à 12h (ainsi que les après-midi)
+          </span>
+          <br>
+          <span>
+            Le samedi de 10h à 13h
+          </span>
+        </span>
+      </div>
+    </div>
+    <div class="agences__content agences__content-two">
+      <p class="agences--title">abel courlancy</p>
+      <p class="agences--adresses">
+        2 rue de Courlancy
+        <br>
+        51100 reims
+        <br>
+        <a href="tel:+0326404533">03.26.40.45.33</a>
+        <br>
+        <span>
+          Du Lundi au Vendredi de 10h à 18h30
+        </span>
+        <br>
+        <span>
+          Le samedi de 10h à 13h
+        </span>
+        <br>
+        <span>
+          Code avec Moniteur le Vendredi à 18h
+        </span>
+      </p>
+    </div>
+    <div class="agences__content agences__content-tree">
+      <p class="agences--title">abel venise</p>
+      <p class="agences--adresses">
+        41 rue Paul Adam (ou 26 rue de Venise)
+        <br>
+        51100 reims
+        <br>
+        <a href="tel:+0326252565">03.26.25.25.65</a>
+        <br>
+        <span>
+          Le lundi de 14h à 19h
+        </span>
+        <br>
+        <span>
+          Du mardi au vendredi de 10h à 13h et de 14h à 19h </span>
+        <br>
+        <span>
+          Code avec Moniteur le Jeudi à 18h
+        </span>
+      </p>
+    </div>
+    <div class="agences__content agences__content-four">
+      <p class="agences--title">abel taissy</p>
+      <p class="agences--adresses">
+        12 rue de Sillery
+        <br>
+        51100 reims
+        <br>
+        <a href="tel:+0326362323">03.26.36.23.23</a>
+        <br>
+        <span>
+          Le Lundi, Mercredi et Vendredi de 17h15 à 18h45
+        </span>
+        <br>
+        <span>
+          Code avec Moniteur le Mercredi à 18h
+        </span>
+      </p>
+    </div>
+
+  </div>
+</section>
+
+
+
+<?php get_footer() ?>
